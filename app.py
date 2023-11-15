3a2a0cbb0184b7e042e93b388f47bdb6f4f5ec3a
r021n <rozinabdul25@gmail.com>
2023-11-15 21:32:51 +0700

    first upload

---
+++ app.py
@@ -0,0 +1,30 @@
+# Import library yang diperlukan
+import joblib
+import numpy as np
+from keras.models import load_model
+
+# Muat model
+# Jika model Anda adalah model Keras, gunakan kode berikut:
+model = load_model('model.h5')
+
+# Jika model Anda adalah model scikit-learn, gunakan kode berikut:
+# model = joblib.load('model.pkl')
+
+# Muat CountVectorizer
+vectorizer = joblib.load('vectorizer.pkl')
+
+# Misalkan kita memiliki teks baru
+teks_baru = ['contoh kalimat yang ingin diprediksi']
+
+# Ubah teks baru menjadi vektor
+teks_baru_vec = vectorizer.transform(teks_baru)
+
+# Gunakan model untuk memprediksi
+prediksi = model.predict(teks_baru_vec)
+
+# Jika model Anda adalah model Keras, gunakan kode berikut untuk mendapatkan kelas prediksi:
+kelas_prediksi = np.argmax(prediksi)
+
+# Jika model Anda adalah model scikit-learn, prediksi sudah dalam bentuk kelas, jadi Anda tidak perlu menggunakan np.argmax
+
+print(f'Prediksi untuk teks baru: {prediksi}')
